13045620b068ecf7c62b05939e62c5ebc6cd3eca
Pedro <pedrohenriqueramossouza@gmail.com>
2014-08-17 18:13:03 +1000

    Implementing YoCatchModel Adding git.txt

--- src/com/uow/YoCatch/MainActivity.java
+++ src/com/uow/YoCatch/MainActivity.java
@@ -1,20 +1,42 @@
 package com.uow.YoCatch;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        YoCatchModel model = new YoCatchModel("Yo");
+        Log.v("msg", model.defaultYoMessage());
+        
+        model.addToHistory("This");
+        model.addToHistory("is");
+        model.addToHistory("a");
+        model.addToHistory("history!");
+        
+         
+        for (int i = 0; i < model.getHistoryArray().size(); i++) {
+			
+			Log.v("msg", i+": "+model.getHistoryArray().get(i));
+		}
+        
         setContentView(R.layout.activity_main);
     }
-
-
+    
+    @Override
+    protected void onStart() {
+    	// TODO Auto-generated method stub
+    	super.onStart();
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
